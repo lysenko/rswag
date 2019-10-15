d55d162e3e5e466e19ee78530af4582f3961fc43
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2019-10-14 17:42:09 -0700

    update rswag spec template

--- rswag-specs/lib/generators/rspec/swagger/templates/spec.rb
+++ rswag-specs/lib/generators/rspec/swagger/templates/spec.rb
@@ -1,29 +1,46 @@
 require 'swagger_helper'
+<% name= controller_path.split('/')[2].try!(:singularize).try!(:titleize) %>
+RSpec.describe '<%= name %>', type: :request do
+  let(:account) { singleton_account }
+  let(:customer) { account.customers.first }
+  let(:api_key) { account.users.first.api_key }
 
-RSpec.describe '<%= controller_path %>', type: :request do
-  <% @routes.each do | template, path_item | %>
-  path '<%= template %>' do
-<% unless path_item[:params].empty? -%>
-    # You'll want to customize the parameter types...
-  <% path_item[:params].each do |param| -%>
-    parameter '<%= param %>', in: :body, type: :string
-  <% end -%>
-<% end -%>
+  <% @routes.sort_by{|route| route.first.size}.each do | template, path_item | %>
+  path '<%= template.gsub('/api/v1','') %>' do
 <% path_item[:actions].each do | action, details | -%>
-    <%= action %>('<%= details[:summary] %>') do
-      response(200, 'successful') do
-<% unless path_item[:params].empty? -%>
-      <% path_item[:params].each do |param| -%>
-        let(:<%= param %>) { '123' }
-      <% end -%>
+    <%= action %> '<%= details[:summary] %>'  do
+      tags '<%=name %>'
+      security [apiKey: []]
+      consumes 'application/json'
+      <% unless path_item[:params].empty? -%>
+      # You'll want to customize the parameter types...
+<% path_item[:params].each do |param| -%>
+      parameter name: '<%= param %>', in: :path, type: :integer
+<% end -%>
 <% end -%>
 
-        after do |example|
-          example.metadata[:response][:examples] = { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
+      response '200', 'successful' do
+        description ""
+<% unless path_item[:params].empty? -%>
+
+<% path_item[:params].each do |param| -%>
+        let(:<%= param %>) { 123 }
+<% end -%>
+<% end -%>
+
+        run_test! do |response|
+          data = JSON.parse(response.body)
         end
-        run_test!
+      end
+
+      response '404', 'Invalid request' do
+        let(:id) { -1 }
+        run_test! do |response|
+          data = JSON.parse(response.body)
+        end
       end
     end
+
 <% end -%>
   end
 <% end -%>
